2908f84d35cf8e66c0f98a85a188a4e25652f9ba
MohammadAlHabil <mohammadalhabil1999@gmail.com>
2022-02-09 13:15:38 +0200

    create get and post products functions >relates #27

--- js/app.js
+++ js/app.js
@@ -6,6 +6,32 @@ const cartSection = document.getElementById("cart-section");
 const btnCloseCart = document.getElementById("btn-close-cart");
 const btnShowCart = document.getElementById("btn-shopping-bag");
 
+//Import data from JSON files
+let fetchData = () => {
+    fetch("../data/products.json")
+        .then((response) => {
+            return response.json();
+        })
+        .then((productListJson) => render(productListJson));
+};
+fetchData();
+
+// Function post Products to localStorage
+function postProducts(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+    getProducts(key);
+}
+
+// Function get Products from localStorage
+function getProducts(key) {
+    data =
+        localStorage.getItem(key) === null
+            ? []
+            : JSON.parse(localStorage.getItem(key));
+    render(data);
+    return data;
+}
+
 // Function  close Modal
 closeModal.addEventListener("click", () => {
     modalStarter.classList.toggle("modal-hidden");
@@ -25,3 +51,15 @@ btnCloseCart.addEventListener("click", () => {
 btnShowCart.addEventListener("click", () => {
     cartSection.classList.toggle("cart-hidden");
 });
+
+// Function Check if the product list has a product
+function hasProduct(productList) {
+    if (
+        productList.length == 0 ||
+        productList == undefined ||
+        productList == null
+    ) {
+        return false;
+    }
+    return true;
+}
