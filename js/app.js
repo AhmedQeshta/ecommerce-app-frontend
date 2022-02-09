39adc6d91659bac8c20b5c41dd0e0863a58bbf69
israaHamdi <israaaburayya2012@gmail.com>
2022-02-09 13:17:05 +0200

    add render cart function RELATES#30

--- js/app.js
+++ js/app.js
@@ -9,6 +9,11 @@ const modalCreateProduct = getElement("modal-create-product");
 const btnCloseProductModal = getElement("close-product-modal");
 const btnAddProduct = getElement("btnAddProduct");
 const modalProductOverview = getElement("modal-product-overview");
+const cartContent=document.querySelector('.content-cart')
+const minusBtn=document.querySelector('.minus')
+const plusBtn=document.querySelector('.plus')
+
+let cartItems=[]
 
 // Function  close Modal
 toggleClass(closeModal, modalStarter, "modal-hidden");
@@ -38,3 +43,38 @@ function toggleClass(element, section, className) {
 function getElement(idName) {
     return document.getElementById(idName);
 }
+
+const renderCart= () => {
+    if(cartItems.length === 0) cartContent.innerHTML ='There is no items yet'
+    else {
+        cartItems.forEach((item)=>{
+            cartContent.innerHTML += ` <div class="item">
+            <img
+                src="${item.image}"
+                alt="${item.name}"
+            />
+            <div class="info-item">
+                <div class="data">
+                    <h3 class="name-item">
+                    ${item.name}
+                    </h3>
+                    <h3 class="price-item">$ ${item.price}</h3>
+                </div>
+                <div class="data">
+                    <div class="quentity">
+                        <button class="minus" >
+                            <i class="far fa-minus"></i>
+                        </button>
+                        <span class="quantity-number">1</span>
+                        <button class="plus" >
+                            <i class="far fa-plus"></i>
+                        </button>
+                    </div>
+                    <h4 class="remove">Remove</h4>
+                </div>
+            </div>
+        </div>`
+        })
+    }
+}
+renderCart()
